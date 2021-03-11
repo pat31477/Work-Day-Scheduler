93f09c3e663ddf32b88e97546bb4b8a4bd9dbedc
Patrick Walker <pat31477@yahoo.com>
2021-03-11 13:42:48 -0500

    added comment to js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,11 @@
+// Display current day and date
+//let todayDate = moment().format('dddd, MMM Do YYYY, h:mm:ss a');
+//$("#currentDay").html(todayDate);
+
 let updateTime = function () {
-    let currentTime = moment().format("dddd, MMMM Do YYYY")
+    let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
     $("#currentDay").text(currentTime)
   }
+
+  setInterval(updateTime, 1000);
 
